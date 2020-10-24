2cd35acb06f8718cf1d19a0758ab9d2870b27d5b
aylhttd <aylhttd@gmail.com>
2020-10-24 17:03:26 +0300

    main

--- tokenizer/tokenizer.cpp
+++ tokenizer/tokenizer.cpp
@@ -1,20 +1,17 @@
-﻿// tokenizer.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include "lemmatizator.h"
 
-#include <iostream>
+vector<fs::path> get_input_texts() {
+	auto input_path = TEXTS_PATH/*fs::current_path()/"inputfiles"*/;
+	fs::recursive_directory_iterator begin(input_path);
+	fs::recursive_directory_iterator end;
+	std::vector<fs::path> txtFiles;
+	std::copy_if(begin, end, std::back_inserter(txtFiles), [](const fs::path& path) {
+		return fs::is_regular_file(path) && (path.extension() == ".txt" || path.extension() == ".TXT"); });
+	return txtFiles;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	auto vector_of_texts = get_input_texts();.
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
